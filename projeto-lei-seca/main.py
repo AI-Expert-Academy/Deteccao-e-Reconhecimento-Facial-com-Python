3f4ec328d0fcd5d13b848e73f1775c45c10fd6b8
João Paulo Maida <jpmaida@gmail.com>
2025-06-25 21:43:57 -0300

    Solução detectando faces e com tratamento caso detecção possua confiança abaixo de 50%

--- projeto-lei-seca/main.py
+++ projeto-lei-seca/main.py
@@ -6,27 +6,6 @@ import streamlit as st
 
 from PIL import Image
 
-def detectaFacesSSDUsandoPath(net, path_imagem, tamanho = 300):
-  imagem = cv2.imread(path_imagem)
-  (h, w) = imagem.shape[:2]
-  blob = cv2.dnn.blobFromImage(cv2.resize(imagem, (tamanho,tamanho)), 1.0, (tamanho,tamanho), (104.0, 177.0, 123.0))
-  net.setInput(blob)
-  deteccoes = net.forward()
-  conf_min = 0.5
-
-  for i in range(0, deteccoes.shape[2]):
-    confianca = deteccoes[0,0,i,2]
-    
-    if confianca > conf_min:
-      box = deteccoes[0,0,i,3:7] * np.array([w,h,w,h])
-      (startX, startY, endX, endY) = box.astype("int")
-      
-      text_conf = "{:.2f}%".format(confianca * 100)
-      print(text_conf)
-      cv2.rectangle(imagem, (startX, startY), (endX, endY), (0,255,0), 2)
-      cv2.putText(imagem, text_conf, (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
-      cv2.imwrite(path_imagem+'_temp.jpg', imagem)
-
 def detectaFacesSSD(net, imagem, tamanho = 300):
   (h, w) = imagem.shape[:2]
   blob = cv2.dnn.blobFromImage(cv2.resize(imagem, (tamanho,tamanho)), 1.0, (tamanho,tamanho), (104.0, 177.0, 123.0))
@@ -47,14 +26,16 @@ def detectaFacesSSD(net, imagem, tamanho = 300):
       cv2.putText(imagem, text_conf, (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
 
       return imagem
+    else:
+      print("Nenhuma face detectada com confiança suficiente.")
+      return None
+
 
 arquivo_modelo = './res10_300x300_ssd_iter_140000.caffemodel'
 arquivo_prototxt = './deploy.prototxt.txt'
 network = cv2.dnn.readNetFromCaffe(arquivo_prototxt, arquivo_modelo)
 
-# detectaFacesSSD(network, 'eu.jpg')
-
-st.title("Captura de imagem com câmera (Streamlit + OpenCV)")
+st.title("Lei Seca do Futuro - Detecção de Faces e placas")
 
 # Interface da câmera
 img_file_buffer = st.camera_input("Tire uma foto")
@@ -67,13 +48,12 @@ if img_file_buffer is not None:
     # Mostra a imagem capturada
     st.image(img_array, caption="Imagem capturada", use_container_width=True)
 
-    # (Opcional) Processamento com OpenCV
-    # st.write("Processamento com OpenCV:")
-    # gray = cv2.cvtColor(img_array, cv2.COLOR_RGB2GRAY)
-    # st.image(gray, caption="Imagem em tons de cinza", use_column_width=True)
-
+    # Mostra a face detectada
     st.write("Detecção de face:")
-
-    newImage = detectaFacesSSD(network, img_array)
-    newImageArray = np.array(newImage)
-    st.image(newImageArray, caption="Imagem capturada", use_container_width=True)
+    faceDetected = detectaFacesSSD(network, img_array)
+    if faceDetected is not None:
+      faceDetectedArray = np.array(faceDetected)
+      st.image(faceDetectedArray, caption="Rosto detectado", use_container_width=True)
+    else:
+      st.write("Nenhuma face detectada.")
+    
