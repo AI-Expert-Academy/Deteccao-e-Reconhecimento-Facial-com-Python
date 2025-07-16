28d38914c6463099e4b1d4baebf68f704ee650ce
João Paulo Maida <jpmaida@gmail.com>
2025-07-15 22:42:14 -0300

    Rename de variáveis para melhor nome, reconhecimento baseado em base de dados, função para reconhecimento de face usando modelo CNN, redimensionamento de imagem tratada para 300x300, tratamento para exibição do que foi capturado

--- projeto-lei-seca/main.py
+++ projeto-lei-seca/main.py
@@ -1,4 +1,7 @@
 import cv2 # OpenCV
+import face_recognition
+import faiss
+import imutils
 import numpy as np
 import tempfile
 import os
@@ -6,9 +9,13 @@ import streamlit as st
 
 from PIL import Image
 
-def detectaFacesSSD(net, imagem, tamanho = 300):
-  (h, w) = imagem.shape[:2]
-  blob = cv2.dnn.blobFromImage(cv2.resize(imagem, (tamanho,tamanho)), 1.0, (tamanho,tamanho), (104.0, 177.0, 123.0))
+index = faiss.read_index("./database/cnh_imgs.faiss")
+imgs_cnh = os.listdir('./database/imgs/')
+
+def detectaFacesSSD(net, foto):
+  tamanho = 300
+  (h, w) = foto.shape[:2]
+  blob = cv2.dnn.blobFromImage(foto, 1.0, (tamanho,tamanho), (104.0, 177.0, 123.0))
   net.setInput(blob)
   deteccoes = net.forward()
   conf_min = 0.5
@@ -21,15 +28,23 @@ def detectaFacesSSD(net, imagem, tamanho = 300):
     if confianca > conf_min:
       box = deteccoes[0,0,i,3:7] * np.array([w,h,w,h])
       (startX, startY, endX, endY) = box.astype("int")
+      (startX, startY, endX, endY) = [int(v) for v in (startX, startY, endX, endY)]
 
-      cv2.rectangle(imagem, (startX, startY), (endX, endY), (0,255,0), 2)
-      cv2.putText(imagem, text_conf, (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+      cv2.rectangle(foto, (startX, startY), (endX, endY), (0,255,0), 2)
+      cv2.putText(foto, text_conf, (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
 
-      return imagem
+      return foto, text_conf
     else:
       print("Nenhuma face detectada com confiança suficiente.")
-      return None
-
+      return None, "0.00%"
+    
+def reconheceFace(face_300x300):
+  face_query_rgb = cv2.cvtColor(face_300x300, cv2.COLOR_BGR2RGB)
+  localizacao_face = face_recognition.face_locations(face_query_rgb, model='cnn')
+  query_enconding_face = face_recognition.face_encodings(face_query_rgb, localizacao_face)[0].astype("float32").reshape(1, -1)
+  k = 3  # número de matches
+  distances, indices = index.search(query_enconding_face, k)
+  return distances, indices
 
 arquivo_modelo = './res10_300x300_ssd_iter_140000.caffemodel'
 arquivo_prototxt = './deploy.prototxt.txt'
@@ -44,16 +59,40 @@ if img_file_buffer is not None:
     # Converte para imagem OpenCV
     img = Image.open(img_file_buffer)
     img_array = np.array(img)
+    
+    img_array_300x300 = imutils.resize(img_array, width=300, height=300)
 
     # Mostra a imagem capturada
     st.image(img_array, caption="Imagem capturada", use_container_width=True)
 
+    # 2 Containers lado a lado
+    colDeteccao, colReconhecimento = st.columns(2)
+
+    faceDetectadaComBoundingBox = None
+
     # Mostra a face detectada
-    st.write("Detecção de face:")
-    faceDetected = detectaFacesSSD(network, img_array)
-    if faceDetected is not None:
-      faceDetectedArray = np.array(faceDetected)
-      st.image(faceDetectedArray, caption="Rosto detectado", use_container_width=True)
-    else:
-      st.write("Nenhuma face detectada.")
-    
+    with colDeteccao:
+      st.write("Detecção de face:")
+      faceDetectadaComBoundingBox, textoConfianca = detectaFacesSSD(network, img_array_300x300)
+      faceDetectadaArray = np.array(faceDetectadaComBoundingBox)
+      if faceDetectadaComBoundingBox is not None:
+        caption = f"Rosto detectado com {textoConfianca} confiança"
+        st.image(faceDetectadaArray, caption=caption)
+        with colReconhecimento:
+          st.write("Rosto reconhecido:")
+          distancias, indices = reconheceFace(img_array_300x300)
+          
+          # Debug output
+          for rank, idx in enumerate(indices[0]):
+            dist = distancias[0][rank]
+            print(f"Match #{rank+1}: (distância: {np.sqrt(dist):.4f}) - Índice: {idx}")
+          
+          distanciasNormalizadas = [np.sqrt(dist) for dist in distancias[0]]
+          index_of_min = np.argmin(distanciasNormalizadas)
+          if distanciasNormalizadas[index_of_min] > 0.6:
+            st.write("Nenhum rosto reconhecido com confiança suficiente.")
+          else:
+            caption = f"Match com probabilidade(distância) de {distanciasNormalizadas[index_of_min]*100:.2f}% em potencial com base dados"
+            st.image(f"./database/imgs/{imgs_cnh[index_of_min]}", caption=caption)
+      else:
+        st.write("Nenhuma face detectada.")
